77269809012c02dafc434776cad9bd472727f9f6
Jason Moore <jmoore2617@gmail.com>
2024-03-08 13:05:08 +0000

    Updates

---
+++ precache-manifest.4c47e96baab371de4367c7c9c0861c8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da5d35d1a6086d50a30c085f9ab1b3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f5764fdfb73a868951f",
+    "url": "/static/js/2.544a6eae.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.544a6eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c8bd9219c0df52a08d",
+    "url": "/static/js/main.a11bc523.chunk.js"
+  },
+  {
+    "revision": "46c89118f843e9d6965e",
+    "url": "/static/js/runtime-main.3090754f.js"
+  }
+]);
